7706ee8b99665afed428e9c9f3221d34a8816d8a
avirup2154 <66943216+avirup2154@users.noreply.github.com>
2026-02-13 23:39:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal 
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif",  // 7 crying runaway
+    "https://media.tenor.com/pdv5YIMB-hkAAAAj/cute.gif",    // 0 normal 
+    "https://media.tenor.com/moRhfDWGAIYAAAAi/bubu-dudu.gif",  // 1 confused
+    "https://media1.tenor.com/m/9fscv__vkukAAAAC/dudu-bubu-dudu-bubu-love.gif",             // 2 pleading
+    "https://media.tenor.com/eSMmyYMJXOcAAAAi/miss-you.gif",             // 3 sad
+    "https://media.tenor.com/j7bxg0MNS38AAAAj/bubu-dudu-sseeyall.gif",       // 4 sadder
+        "https://media.tenor.com/zxUCBttQRnMAAAAi/twitter-gun.gif",             // 5 devastated
+    
+    "https://media.tenor.com/BvlQdl0TAeIAAAAi/cute.gif",               // 6 very devastated
+    
+    "https://media.tenor.com/1pId72nvliQAAAAj/toory-dudu-bubu-osita.gif",  // 7 crying runaway
     "https://media.tenor.com/9sadt8tWJuUAAAAi/lp.gif"
 ]
 
